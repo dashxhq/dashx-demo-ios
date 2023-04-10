6862f49dd340792aad54294a65f6e9ebf4e5caa8
Amandeep Singh Ghai <Amandeepsinghghai@users.noreply.github.com>
2023-04-09 23:34:03 -0700

    Cleanup messaging (#8)
    * Cleanup AppDelgate and fix API calls
    
    * Call track after setIdentity to ensure events are tracked properly
    
    * Update pod version
    
    * Update to version 1.0.10

--- ViewControllers/MoreItemsViewController.swift
+++ ViewControllers/MoreItemsViewController.swift
@@ -4,55 +4,56 @@
 //
 //  Created by Ravindar Katkuri on 01/07/22.
 //
-import UIKit
 import DashX
+import UIKit
 
 typealias MoreScreenDataItem = (title: String, action: () -> Void)
 
 class MoreItemsViewController: UIViewController {
     static let identifier = "MoreItemsViewController"
     @IBOutlet var tableView: UITableView!
-    
+
     var screenItems: [MoreScreenDataItem] = []
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         screenItems = [
-            (title: "Billing", action: { return }),
+            (title: "Billing", action: {}),
             (title: "Profile", action: goToUpdateProfileScreen),
             (title: "Settings", action: goToSettingsScreen),
             (title: "Logout", action: showAlertToLogoutConfirmation)
         ]
-        
+
         // Setup tableview
         tableView.delegate = self
         tableView.dataSource = self
     }
-    
+
     // MARK: ViewWillAppear
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setupThemedNavigationBar(for: traitCollection.userInterfaceStyle)
     }
-    
+
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         setupThemedNavigationBar(for: traitCollection.userInterfaceStyle)
     }
-    
+
     func goToUpdateProfileScreen() {
         let updateProfileVC = UIViewController.instance(of: UpdateProfileViewController.identifier)
-        self.navigationController?.pushViewController(updateProfileVC, animated: true)
+        navigationController?.pushViewController(updateProfileVC, animated: true)
     }
-    
+
     func goToSettingsScreen() {
         let settingsVC = UIViewController.instance(of: SettingsViewController.identifier)
-        self.navigationController?.pushViewController(settingsVC, animated: true)
+        navigationController?.pushViewController(settingsVC, animated: true)
     }
-    
+
     func showAlertToLogoutConfirmation() {
         let logoutConfirmationAlert = UIAlertController(title: "Confirm Logout", message: "Are you sure you wish to logout?", preferredStyle: .alert)
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: {_ in
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in
             logoutConfirmationAlert.dismiss(animated: true)
         })
         let logoutAction = UIAlertAction(title: "Okay", style: .destructive, handler: { _ in
@@ -60,19 +61,20 @@ class MoreItemsViewController: UIViewController {
         })
         logoutConfirmationAlert.addAction(cancelAction)
         logoutConfirmationAlert.addAction(logoutAction)
-        self.present(logoutConfirmationAlert, animated: true)
+        present(logoutConfirmationAlert, animated: true)
     }
-    
+
     func performLogout() {
         // Induce artificial "processing" gap instead of snappy logout
-        self.showProgressView()
+        showProgressView()
+
         DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
             // Clear local storage keys
             LocalStorage.instance.clearAll()
+
             DashX.reset()
-            
             self.hideProgressView()
-            
+
             let loginVC = UIViewController.instance(of: LoginViewController.identifier)
             let navVC = UINavigationController(rootViewController: loginVC)
             self.view.window?.rootViewController = navVC
@@ -84,7 +86,7 @@ extension MoreItemsViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let screenItemIndex = indexPath.row
         let onClickAction = screenItems[screenItemIndex].action
-        
+
         onClickAction()
     }
 }
@@ -93,20 +95,20 @@ extension MoreItemsViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return screenItems.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let screenItemIndex = indexPath.row
         let cellTitle = screenItems[screenItemIndex].title
-        
+
         let cell = tableView.dequeueReusableCell(withIdentifier: "MoreScreenTableViewCell",
                                                  for: indexPath) as! MoreScreenTableViewCell
-        
+
         cell.titleLabel.text = cellTitle
-        
+
         return cell
     }
 }
